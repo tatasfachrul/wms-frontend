50e6b5acb7e8016ed2ab71e4bfe58ec250c98837
Tatas Fachrul <tatasfachrul@gmail.com>
2025-11-12 11:18:51 +0700

    upgrade middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
   const isAuthPage = request.nextUrl.pathname.startsWith('/login');
   const isProtectedPage = request.nextUrl.pathname.startsWith('/dashboard') ||
